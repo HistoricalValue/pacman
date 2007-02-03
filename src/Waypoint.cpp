5de421379ca42a45ef1b70d28b9960d9d6e39e30
Saku Etanainen <isilando@shadowblink>
2007-02-03 18:33:26 +0200

    * Added Waypoint

---
+++ src/Waypoint.cpp
@@ -0,0 +1,18 @@
+#include "Waypoint.hpp"
+
+Waypoint::Waypoint(int _x, int _y, bool _up, bool _down, 
+		   bool _left, bool _right, spriteid_t id) 
+		   : Sprite( _x, _y, NULL, id)
+{
+	up = _up;
+	down = _down;
+	left = _left;
+	right = _right;
+}
+
+bool Waypoint::CollisionCheck(Sprite *s) {
+	//Absolute collision
+	if(x == s->GetX() && y == GetY())
+		return true;
+	return false;
+}
