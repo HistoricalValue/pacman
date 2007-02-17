580756830bb59081c638fce89863320de9aae813
Saku Etanainen <isilando@shadowblink>
2007-02-17 05:26:27 +0200

    Added input control files

---
+++ client/inputControl.hpp
@@ -0,0 +1,9 @@
+/* Handle input events functions. */
+
+#ifndef __inputControl_hpp__
+#define __inputControl_hpp__
+
+#include "client.hpp"
+
+extern void inputControl(GameData &, bool *);
+#endif // __inputControl_hpp__
