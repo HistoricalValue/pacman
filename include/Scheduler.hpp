4e11b9a04c3da9d300003b9c65356f6510883215
Saku Etanainen <isilando@shadowblink>
2007-02-24 15:27:31 +0200

    Might have a working scheduler

--- include/Scheduler.hpp
+++ include/Scheduler.hpp
@@ -11,7 +11,7 @@
 #include <list>
 #include "Animator.hpp" // for timestamp_t
 
-typedef struct Task *task_t;
+typedef class Task *task_t;
 typedef struct TaskData *taskdata_t;
 
 class Scheduler {
@@ -21,11 +21,27 @@ class Scheduler {
 	status_t track(task_t const&);
 	bool cancel(task_t const&);
 	void check(timestamp_t currTime);
-	Scheduler(void);
+	Scheduler(timestamp_t starting_time);
 	~Scheduler(void);
 	private :
+	struct TaskExecutor;
 	std::map<task_t, taskdata_t> tasks;
 	std::list<task_t> times;
+	TaskExecutor *executor;
 }; // class Scheduler
+
+class Task {
+	public :
+	virtual bool operator==(Task const*) const = 0;
+	virtual void operator()(taskdata_t) const = 0;
+	virtual Task&operator++(void) = 0;
+	Task(timestamp_t diff, bool recurring); virtual ~Task(void);
+	timestamp_t getTime(void) const;
+	bool isRecurring(void) const;
+	private :
+	timestamp_t time;
+	bool recurring;
+}; // class Task
+struct TaskData { };
 
 #endif // __Scheduler_hpp__
--- src/Scheduler.cpp
+++ src/Scheduler.cpp
@@ -1,16 +1,18 @@
 #include "Scheduler.hpp"
 #include <algorithm>
 
-struct TaskData { };
+// Checks for each task if it should be executed and executes it if it
+// should.
+struct Scheduler::TaskExecutor : public std::unary_function<task_t, void> {
+	void operator()(task_t) const;
+	void setCurrentTime(timestamp_t);
+	TaskExecutor(timestamp_t currTime, Scheduler *);
+	private :
+	timestamp_t currTime;
+	Scheduler *sch;
+}; // struct TaskExecutor
 
-struct Task {
-	virtual bool operator==(Task const&) const;
-	virtual bool operator==(Task const&);
-	virtual void operator()(TaskData&);
-	Task(void);
-	virtual ~Task(void);
-};
-
+// Scheduler Implementation ------------------------------------------------
 void Scheduler::_register(task_t const &t, taskdata_t const &d) {
 	// Make sure task is not registered.
 	// (Comparisons are made between pointers and not class IDs so that
@@ -27,5 +29,55 @@ void Scheduler::_register(task_t const &t, taskdata_t const &d) {
 } // Scheduler::_register
 
 void Scheduler::check(timestamp_t currTime) {
-	
+	// Calculate how much time is past from last time (check was run)
+	executor->setCurrentTime(currTime);
+	// Check and run what tasks is needed
+	std::for_each(times.begin(), times.end(), *executor);
 } // Scheduler::check
+
+bool Scheduler::cancel(task_t const &t) {
+	tasks.erase(tasks.find(t));
+	times.remove(t);
+
+	return true;
+} // Scheduler::cancel
+
+// Task Executor Implementation --------------------------------------------
+void Scheduler::TaskExecutor::operator()(task_t t) const {
+	if (t->getTime() >= currTime) { // should run task
+		(*t)(sch->tasks[t]);
+		// if task is no reoccuring, remove from the queue
+		if (!t->isRecurring())
+			sch->cancel(t);
+		else
+			// Advance task's execution time and leave
+			// registered
+			++(*t);
+	}
+} // TaskExecutor::()
+
+void Scheduler::TaskExecutor::setCurrentTime(timestamp_t _currTime) {
+	currTime = _currTime;
+}
+
+// Task Implementation -----------------------------------------------------
+timestamp_t Task::getTime(void) const { return time; }
+bool Task::isRecurring(void) const { return recurring; }
+
+
+// Constructors
+Scheduler::Scheduler(timestamp_t _startingTime) :
+	executor(new TaskExecutor(_startingTime, this)) { }
+Scheduler::TaskExecutor::TaskExecutor(timestamp_t _currTime,Scheduler*_sch):
+	currTime(_currTime),
+	sch(_sch) { }
+
+// Destructors
+Scheduler::~Scheduler(void) {
+	std::map<task_t, taskdata_t>::iterator ite;
+	for (ite = tasks.begin(); ite != tasks.end(); ite++) {
+		delete ite->first;
+		delete ite->second;
+	}
+	delete executor;
+}
