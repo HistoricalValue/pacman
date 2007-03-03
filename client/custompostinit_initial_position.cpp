067cb5e2c05cf226ce165b79bcb88ab8c8a30c6a
Saku Etanainen <isilando@shadowblink>
2007-03-03 18:24:37 +0200

    Initial special sprite positions stored durin setup

--- client/custompostinit_initial_position.cpp
+++ client/custompostinit_initial_position.cpp
@@ -1,6 +1,24 @@
 #include "CustomPostinit_Commons.hpp"
 
-CPI_DEFINE(custompostinit_initial_position) {
+struct PositionSave : public std::unary_function<GameSprite*, void> {
+	result_type operator()(argument_type);
+	PositionSave(std::map<GameSprite*, SDL_Rect> &);
+	private :
+	std::map<GameSprite*, SDL_Rect> &p;
+}; // struct PositionSave
 
+CPI_DEFINE(custompostinit_initial_position) {
+	std::for_each(gd.sss.begin(), gd.sss.end(),
+	 PositionSave(gd.custom->initpos));
+	db("Initial positions -- saved");
 } // custompostinit_inital_position
 
+PositionSave::PositionSave(std::map<GameSprite*, SDL_Rect> &_p) : 
+	p(_p) { }
+
+PositionSave::result_type
+PositionSave::operator()(argument_type ss) {
+	// ss = special sprite (actor)
+	p[ss].x = ss->GetX();
+	p[ss].y = ss->GetY();
+} // PositionSave()
