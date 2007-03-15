0e99e6e1c9c00d6af4bec6acb855944d4c30e239
manousak <manousak@euclides>
2007-03-16 00:33:13 +0200

    Added flashing "press any key" and tasks for 2nd player scheduling.

--- client/StartingScreen.cpp
+++ client/StartingScreen.cpp
@@ -1,9 +1,19 @@
 #include "StartingScreen.hpp"
+#include "SDL/SDL_ttf.h"
 
 void ShowStartingScreen(SDL_Surface *surf, SDL_Surface *screen,
  _bools &bools)
 {
+        bool daflag = true;
+	TTF_Font *ttf_msg(TTF_OpenFont("resources/fonts/Alias.ttf", 32));
+	SDL_Rect pmessage;
+	SDL_Surface *msg; 
+	SDL_Color col;
+	col.r = 0xff; col.g = 0xff; col.b = 0xff;
 	SDL_Event event;
+	pmessage.x = 16; pmessage.y = 550; pmessage.w = 100; pmessage.h = 30;
+	msg = TTF_RenderUTF8_Blended(
+		ttf_msg, "Press any key to Continue", col);
 	while (!bools.start) {
 		if(!SDL_PollEvent(&event))
 			;
@@ -23,6 +33,16 @@ void ShowStartingScreen(SDL_Surface *surf, SDL_Surface *screen,
 		}
 
 		// capping
-		SDL_Delay(17);
+		SDL_Delay(500);
+		if(daflag){
+		  SDL_BlitSurface(msg, NULL, screen, &pmessage);
+		  SDL_Flip(screen);
+		  daflag = false;
+		}
+		else{
+		  SDL_FillRect(screen, &pmessage, 0);
+		  SDL_Flip(screen);
+		  daflag = true;  
+		}
 	}
 }
--- client/second_player.cpp
+++ client/second_player.cpp
@@ -1,7 +1,20 @@
 #include "second_player.hpp"
 
+struct GhostPlayerTaskData : public TaskData {
+  Ghosts &ghost;
+  GameData::io_bools &iob;
+}; // struct GhostRevertTaskData
+
+struct GhostPlayerTask : public Task {
+	void operator()(TaskData *);
+	Task &operator++(void);
+	Task &operator+=(timestamp_t);
+	GhostPlayerTask(timestamp_t time); ~GhostPlayerTask(void);
+}; // struct GhostPlayertTask
+
 void enableGhostInput(Ghosts &ghost, GameData::io_bools &iob) {
 	static uint8_t ghost_select = 0;
+	disableGhostInput(ghost, iob);
 	switch (ghost_select) {
 		case 0: ghost.player2 = ghost.stalker; break;
 		case 1: ghost.player2 = ghost.kieken; break;
@@ -20,3 +33,32 @@ void disableGhostInput(Ghosts &ghost, GameData::io_bools &iob) {
 	iob.second_player = false;
 }// disableGhostInput
 
+/*
+Task &GhostPlayerTask:: public Task {
+	void operator()(TaskData *);
+	Task &operator++(void);
+	Task &operator+=(timestamp_t);
+	GhostRevertTask(timestamp_t time); ~GhostRevertTask(void);
+}; // struct GhostRevertTask
+
+*/
+
+void GhostPlayerTask::operator()(TaskData *tsk){
+  enableGhostInput(
+		   dynamic_cast<GhostPlayerTaskData*>(tsk)->ghost, 
+		   dynamic_cast<GhostPlayerTaskData*>(tsk)->iob
+		   );
+}
+
+Task &GhostPlayerTask::operator++(void){
+  time+=10000;
+}
+
+Task &GhostPlayerTask::operator+=(timestamp_t _t){
+  time+=_t;
+}
+
+GhostPlayerTask::GhostPlayerTask(timestamp_t time) :
+  Task(time, false) { }
+
+GhostPlayerTask::~GhostPlayerTask(){ }
